c4a0930689a12933574140f5a7439c3ff56c0e68
samuelprevedello <smsamuelmelo0@gmail.com>
2025-09-15 17:47:42 -0300

    test: add /health endpoint test

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,11 @@ def test_home():
     resp = client.get('/')
     assert resp.status_code == 200
     assert resp.get_json().get("message") == "Hello, DevOps (Semana 2)!"
+
+def test_health():
+    from app.main import app
+    client = app.test_client()
+    r = client.get('/health')
+    assert r.status_code == 200
+    assert r.get_json().get('status') == 'ok'
+
